7a5e90a0cf41ef5d9382e79baade407c8180cba0
Daniel Zvara <dan.zvara@gmail.com>
2024-02-07 10:08:32 +0100

    Create chat agent

---
+++ app/agent.py
@@ -0,0 +1,67 @@
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_community.chat_models.openai import ChatOpenAI
+from langchain.memory.chat_memory import BaseChatMemory
+import json
+
+from tools import (
+    create_retriever_tool,
+    create_google_search_tool,
+    create_python_repl_tool,
+)
+from prompt import agent_prompt
+
+index_name = "langchain-demo"
+
+
+def create_agent(memory: BaseChatMemory):
+    tools = [
+        create_retriever_tool(index_name),
+        create_google_search_tool(),
+        create_python_repl_tool(),
+    ]
+
+    llm = ChatOpenAI(temperature=0, streaming=True, callbacks=[])
+
+    agent = create_openai_tools_agent(llm, tools, agent_prompt)
+
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        max_iterations=3,
+        verbose=True,
+        return_intermediate_steps=False,
+    )
+
+    return agent_executor
+
+
+async def stream_output_with_annotations(agent: AgentExecutor, inputs):
+    tool_name, source_path = None, None
+    streaming_content = False
+
+    async for event in agent.astream_events(inputs, version="v1"):
+        kind = event["event"]
+
+        if kind == "on_retriever_end":
+            output = event["data"]["output"]
+            documents = output["documents"]
+            if len(documents):
+                source_path = documents[0].metadata["path"]
+
+        if kind == "on_tool_start":
+            tool_name = event["name"]
+
+        if kind == "on_chat_model_stream":
+            content = event["data"]["chunk"].content
+            if content:
+                if not streaming_content:
+                    annotations = {}
+                    if tool_name:
+                        annotations["tool_name"] = tool_name
+                    if source_path:
+                        annotations["source_path"] = source_path
+                    yield "8:" + json.dumps([annotations]) + "\n"
+                    streaming_content = True
+
+                yield "0:" + json.dumps(content) + "\n"
